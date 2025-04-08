4f2f7e302a3d9df99ac571e15dde4422b57cbf01
Guanjino <jjpereiranaranjo@gmail.com>
2025-04-08 16:49:29 +0200

    Configuración inicial: conexión a MongoDB y servidor Express listo

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('API del vivero funcionando')
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor conrriendo en http://localhost:${PORT}`)    
+})
