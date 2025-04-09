049be158bfe702a6b0170278d7b1d85a699b2bb4
Guanjino <jjpereiranaranjo@gmail.com>
2025-04-09 16:43:02 +0200

    Añadido endpoint para filtrar productos por categoría

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,9 +13,14 @@ export const createProduct = async (req, res) => {
 
 export const getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find()
+        const { category } = req.query
+        const filter = category ? { category } : {}
+
+        const products = await Product.find(filter)
         res.status(200).json(products)
     } catch (error) {
+        console.error('Error al obtener productos', error);
+        
         res.status(500).json({ message: 'Error al obtener los productos' })
     }
 }
