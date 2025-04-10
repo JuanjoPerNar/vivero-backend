2c858640f21a1d431d9fd8bf86551a34b96961c6
Guanjino <jjpereiranaranjo@gmail.com>
2025-04-10 19:17:00 +0200

    Integradas rutas de publicaciones en index.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,28 +1,12 @@
-import mongoose from "mongoose"
+import express from 'express'
+import { createPost, getAllPosts, getPostById, updatePost, deletePost } from '../controllers/postControllers'
 
-const postSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    author: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    }
-}, {
-    timestamps: true
-})
+const router = express.Router()
 
-const Post = mongoose.model('Post', postSchema)
+router.post('/', createPost)
+router.get('/', getAllPosts)
+router.get('/:id', getPostById)
+router.put('/:id', updatePost)
+router.delete('/', deletePost)
 
-export default Post
+export default router
