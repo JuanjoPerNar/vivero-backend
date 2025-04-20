44e2b1a3c5d017331fe17b7782179be30d7d5ccf
Guanjino <jjpereiranaranjo@gmail.com>
2025-04-20 20:30:16 +0200

    Añadida documentación Swagger para todos los endpoints

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './config/db.js'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './docs/swagger.js'
+
 import productRoutes from './routes/productRoutes.js'
 import postRoutes from './routes/postRoutes.js'
 import trefleRoutes from './routes/trefleRoutes.js'
@@ -10,12 +13,15 @@ import contactRoutes from './routes/contactRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import serviceRoutes from './routes/serviceRoutes.js'
 
+
 dotenv.config()
 connectDB()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req, res) => {
   res.send('API del vivero funcionando.')
